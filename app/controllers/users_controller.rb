19ebfdfe677f00dfb640b49c9e164a11bdcca645
=Haodong Zheng <=>
2020-03-30 21:46:03 -0500

    good

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -11,10 +11,11 @@ class UsersController < ApplicationController
   def create
     @user = User.new(user_params)    # Not the final implementation!
     if @user.save
+      log_in @user
       flash[:success] = "Welcome to the Sample App"
       redirect_to user_url(@user)
     else
-      render 'new'
+      render 'new'    #render new template
     end
   end
 
